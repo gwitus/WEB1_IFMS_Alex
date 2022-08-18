da16295390c3507bcd15f3e45781babd5f01ba73
Emanuel Barbosa <emanuelbarrbosati@gmail.com>
2022-08-18 01:51:03 -0400

    Js part 1 completed

--- atividade1/script.js
+++ atividade1/script.js
@@ -1,7 +1,22 @@
 var noturno = document.getElementsByTagName("button")[0];
+
 var div = document.getElementsByTagName("main")[0];
+var head = document.getElementsByTagName("header")[0];
+var foot = document.getElementsByTagName("footer")[0]; 
 
 //O erro era pq o JS não estava no final da página, portanto as tags ainda não haviam sido carregadas;
 noturno.onclick = function click() {
-    div.style.backgroundColor = "black";
+    if (div.style.backgroundColor == 'rgb(231, 231, 231)'){
+        div.style.backgroundColor = '#141f27';
+        div.style.color = "white";
+
+        head.style.backgroundColor = '#141f27';
+        foot.style.backgroundColor = '#141f27';
+
+    } else {
+        div.style.backgroundColor = "#e7e7e7";
+        div.style.color = "blue";
+        head.style.backgroundColor = '#e7e7e7';
+        foot.style.backgroundColor = '#e7e7e7';
+    }
 }
