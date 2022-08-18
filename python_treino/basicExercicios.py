8587f357af3bc51821e867352bad88aabd1de253
Emanuel Barbosa <emanuelbarrbosati@gmail.com>
2022-08-18 17:15:25 -0400

    Erro nas condicionais, estudar sobre buffer no python

--- python_treino/basicExercicios.py
+++ python_treino/basicExercicios.py
@@ -1,18 +1,25 @@
 #-----------------------------------------Cabeçalho-------------------------------------------#
+from ast import While
+
+
 url =  'https://wiki.python.org.br/ListaDeExercicios'
-print("Nesse arquivo contém atividades em python realizadas com base no site: " + url)
+print("\nNesse arquivo contém atividades em python realizadas com base no site: " + url)
 
 #-----------------------------Menusinho em python------------------------------#
-exercicioEscolhido = input("Insira qual exercício gostaria de ver funcionando: ")
+coringa = 0;
+
+while coringa <= 15:
+    exercicioEscolhido = input("\nInsira qual exercício gostaria de ver funcionando:")
+
+    if (exercicioEscolhido == 1):
+        #Exercício 1
+        print("Hello world")
+    if(exercicioEscolhido == 2):
+        #Exercicio 2
+        number = input("\nInsira um número: ")
+        print("\nO número inserido foi: " + number)
+    
+    coringa += 1
 
 
-#Exercício 1
-print("Hello world")
 
-#Exercício 2
-
-
-
-a = 3
-b = 2
-print(a + b)
