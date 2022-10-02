0e7759c213da3a39daf469f504c71f429cda9927
Emanuel-Perflex <emanuelbarbosati@gmail.com>
2022-10-02 00:10:02 -0400

    Estrutura do funcionário realizada

---
+++ Atividades Gerais/JPnews/Noticia/imagem.js
@@ -0,0 +1,8 @@
+//Função não funcionou, estudar forma de estilizar o botão futuramente
+
+let photo = document.getElementById('substituto');
+let file = document.getElementById('imagem');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
